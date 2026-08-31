e5a131a32b9df8bda14e6f67f10c820993caa464
unblockedgames99x-code <unblockedgames99x-code@users.noreply.github.com>
2026-08-30 20:50:32 -0500

    Package dedicated YouTube app

--- build-google-script-direct.mjs
+++ build-google-script-direct.mjs
@@ -2,7 +2,7 @@ import { readFile, writeFile } from "node:fs/promises";
 
 const sourcePath = new URL("./neo-os/index.html", import.meta.url);
 const outputPath = new URL("./google-script-direct.html", import.meta.url);
-const revision = "1815ff1";
+const revision = "60ff546";
 const cdnRoot = `https://cdn.jsdelivr.net/gh/unblockedgames99x-code/dshsfdghaswerweq@${revision}/neo-os/`;
 const pageRoot = cdnRoot;
 

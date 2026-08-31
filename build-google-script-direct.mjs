2c1281ade73f58071892de0e6b56507ae09b5309
unblockedgames99x-code <unblockedgames99x-code@users.noreply.github.com>
2026-08-30 20:14:40 -0500

    Package wallpaper import fix for Google Script

--- build-google-script-direct.mjs
+++ build-google-script-direct.mjs
@@ -2,7 +2,7 @@ import { readFile, writeFile } from "node:fs/promises";
 
 const sourcePath = new URL("./neo-os/index.html", import.meta.url);
 const outputPath = new URL("./google-script-direct.html", import.meta.url);
-const revision = "85d2dc6";
+const revision = "ee0d3c8";
 const cdnRoot = `https://cdn.jsdelivr.net/gh/unblockedgames99x-code/dshsfdghaswerweq@${revision}/neo-os/`;
 const pageRoot = cdnRoot;
 
